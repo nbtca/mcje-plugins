3110ab8305e71fd62c66a772458b262fdd45ffb3
LazuliKao <LazuliKao233@outlook.com>
2024-10-07 16:12:09 +0800

    Update PlayerAdvancementTrackerMixin.java

--- message_bridge_fabric/src/main/java/space/nbtca/mc/mixin/PlayerAdvancementTrackerMixin.java
+++ message_bridge_fabric/src/main/java/space/nbtca/mc/mixin/PlayerAdvancementTrackerMixin.java
@@ -1,7 +1,5 @@
 package space.nbtca.mc.mixin;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.advancement.AdvancementEntry;
-import net.minecraft.advancement.AdvancementManager;
 import net.minecraft.advancement.PlayerAdvancementTracker;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.GameRules;
@@ -15,18 +13,15 @@ import space.nbtca.mc.MessageBridgeFabric;
 public class PlayerAdvancementTrackerMixin {
     @Shadow
     private ServerPlayerEntity owner;
-    @Inject(method = "grantCriterion", at = @At("TAIL"))
+    @Inject(method = "grantCriterion", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancement/PlayerAdvancementTracker;onStatusUpdate(Lnet/minecraft/advancement/AdvancementEntry;)V"))
     void grantCriterion(AdvancementEntry advancement, String criterionName, CallbackInfoReturnable<Boolean> ci) {
-        var result = ci.getReturnValue();
-        if (result) {
-            advancement.value().display().ifPresent(display -> {
-                if (display.shouldAnnounceToChat() && this.owner.getWorld().getGameRules().getBoolean(GameRules.ANNOUNCE_ADVANCEMENTS)) {
-                    var title = display.getTitle();
-                    var description = display.getDescription();
-                    var criteria = advancement.value().criteria().keySet().toArray(new String[0]);
-                    MessageBridgeFabric.instance.onAdvancementAchieved(this.owner, title.getString(), description.getString(), criteria);
-                }
-            });
-        }
+        advancement.value().display().ifPresent(display -> {
+            if (display.shouldAnnounceToChat() && this.owner.getWorld().getGameRules().getBoolean(GameRules.ANNOUNCE_ADVANCEMENTS)) {
+                var title = display.getTitle();
+                var description = display.getDescription();
+                var criteria = advancement.value().criteria().keySet().toArray(new String[0]);
+                MessageBridgeFabric.instance.onAdvancementAchieved(this.owner, title.getString(), description.getString(), criteria);
+            }
+        });
     }
 }
